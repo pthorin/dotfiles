73224115362f51235a29691cabc4c681ab9d3384
Peter Thorin <pthorin@volvocars.com>
2022-10-12 21:07:54 +0200

    backup

--- hotplug/usr/local/bin/hotplug_monitor.sh
+++ hotplug/usr/local/bin/hotplug_monitor.sh
@@ -3,7 +3,7 @@
 ######################################
 ## /usr/local/bin/hotplug_monitor.sh
 ######################################
-X_USER=pthorin
+export X_USER=pthorin
 export DISPLAY=:0
 export XAUTHORITY=/home/$X_USER/.Xauthority
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
@@ -17,20 +17,21 @@ function disconnect() {
   xrandr --output "$1" --off
 }
 
-if [ $(cat /sys/class/drm/card0-**/status|grep -c ^connected) -eq 1 ]; then
-  xrandr --auto
+if [ "$(cat /sys/class/drm/card0-**/status|grep -c ^connected)" -eq 1 ]; then
+  xrandr --auto --output eDP-1 --scale 2
 else
   disconnect eDP-1
   MONITORS=(DP-1 DP-2 DP-3 DP-4 DP-5 DP-6)
-  for MONITOR in $MONITORS; do
-    if [ $(cat /sys/class/drm/card0-$MONITOR/status) == "connected" ] ; then
-      connect $MONITOR
-    elif [ $(cat /sys/class/drm/card0-$MONITOR/status) == "disconnected" ] ; then
-      disconnect $MONITOR
+  for MONITOR in "${MONITORS[@]}"; do
+    if [ "$(cat /sys/class/drm/card0-"$MONITOR"/status)" == "connected" ] ; then
+      connect "$MONITOR"
+    elif [ "$(cat /sys/class/drm/card0-"$MONITOR"/status)" == "disconnected" ] ; then
+      disconnect "$MONITOR"
     else
       exit
     fi
   done
 fi
 
-feh --bg-fill /home/pthorin/.local/share/backgrounds/IMG_0130.jpg
+# shellcheck source=/home/pthorin/.fehbg
+. /home/pthorin/.fehbg
--- scripts/headphones.sh
+++ scripts/headphones.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-pactl set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-headphones &&
+#pactl set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-headphones &&
+pactl set-sink-port bluez_sink.E4_41_22_13_88_57.a2dp_sink &&
   touch /tmp/headphones
 
