21c9c9705109d78b80f268327391190bf835841d
Peter Thorin <pthorin@volvocars.com>
2023-08-16 11:54:11 +0200

    latest changes

--- hotplug/usr/local/bin/hotplug_monitor.sh
+++ hotplug/usr/local/bin/hotplug_monitor.sh
@@ -10,7 +10,7 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 
 function connect() {
     #dual monitor - HDMI1 at right - xfce panel on left (LVDS1)
-    xrandr --output "$1" --auto --primary >> /tmp/screen
+    xrandr --output "$1" --auto --scale 1 --primary --above eDP-1 >> /tmp/screen
 }
 
 function disconnect() {
@@ -20,7 +20,6 @@ function disconnect() {
 if [ "$(cat /sys/class/drm/card0-**/status|grep -c ^connected)" -eq 1 ]; then
   xrandr --auto --output eDP-1 --scale 2
 else
-  disconnect eDP-1
   MONITORS=(DP-1 DP-2 DP-3 DP-4 DP-5 DP-6)
   for MONITOR in "${MONITORS[@]}"; do
     if [ "$(cat /sys/class/drm/card0-"$MONITOR"/status)" == "connected" ] ; then
@@ -34,4 +33,4 @@ else
 fi
 
 # shellcheck source=/home/pthorin/.fehbg
-. /home/pthorin/.fehbg
+/home/$X_USER/.fehbg
--- scripts/headphones.sh
+++ scripts/headphones.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 #pactl set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-headphones &&
-pactl set-sink-port bluez_sink.E4_41_22_13_88_57.a2dp_sink &&
+# pactl set-sink-port bluez_sink.E4_41_22_13_88_57.a2dp_sink &&
+set -x
+pactl set-source-port alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source \[In\]\ Mic2 ;
+pactl set-sink-port alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink \[Out\]\ Headphones ;
+pactl set-default-sink alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink ;
+  pactl set-default-source alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source ;
   touch /tmp/headphones
 
