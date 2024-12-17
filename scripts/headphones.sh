4ae149e1f25239e0182c53b8b12e429bf27b5fcc
Peter Thorin <pthorin@volvocars.com>
2024-12-17 08:50:41 +0100

    feat(headphones): set headphones / mic as default and move all i/o there

--- scripts/headphones.sh
+++ scripts/headphones.sh
@@ -5,6 +5,17 @@ set -x
 pactl set-source-port alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source \[In\]\ Mic2 ;
 pactl set-sink-port alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink \[Out\]\ Headphones ;
 pactl set-default-sink alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink ;
-  pactl set-default-source alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source ;
-  touch /tmp/headphones
+pactl set-default-source alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source ;
 
+
+for x in $(pactl list sink-inputs short | cut -d' ' -f1); 
+do
+  pactl move-sink-input $x alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink ;
+done
+
+for x in $(pactl list source-outputs short | cut -d' ' -f1);
+do
+  pactl move-source-output $x alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source ;
+done
+
+touch /tmp/headphones
