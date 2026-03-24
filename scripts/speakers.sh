9ff4b3ad3564cab6edaf168a780f780c94553b52
Peter Thorin <pthorin@volvocars.com>
2026-03-24 11:38:20 +0100

    move output to speakers

--- scripts/speakers.sh
+++ scripts/speakers.sh
@@ -2,16 +2,16 @@
 #pactl set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-headphones &&
 # pactl set-sink-port bluez_sink.E4_41_22_13_88_57.a2dp_sink &&
 set -x
-pactl set-source-port alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source \[In\]\ Mic2 ;
-pactl set-sink-port alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink \[Out\]\ Speakers ;
+#pactl set-source-port alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source \[In\]\ Mic2 ;
+pactl set-sink-port alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink \[Out\]\ Speaker ;
 pactl set-default-sink alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink ;
-pactl set-default-source alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source ;
+#pactl set-default-source alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source ;
 
 #  awk '{ print $1 }'
-for x in $(pactl list sink-inputs short | awk '{ print $1 }'); 
-do
-  pactl move-sink-input $x alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink ; 
-done
+# for x in $(pactl list sink-inputs short | awk '{ print $1 }'); 
+# do
+#   pactl move-sink-input $x alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink ; 
+# done
 
 for x in $(pactl list source-outputs short | awk '{ print $1 }');
 do
